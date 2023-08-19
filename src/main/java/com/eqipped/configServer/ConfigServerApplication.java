3e9f8edb0b5738138e1c8ec02b07f93d94161f0c
Aman-kumar-1999 <Aman-kumar-1999>
2023-08-19 17:25:03 +0530

    V1 Config Server

---
+++ src/main/java/com/eqipped/configServer/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.eqipped.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ConfigServerApplication.class, args);
+		System.out.println("CONFIG_SERVER Application is Running ... ");
+	}
+
+}
